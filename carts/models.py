98783966b39100abe96b8d95cbe981e783303d30
kang min sung <kangminsung@gangminseong-ui-MacBookPro.local>
2021-12-28 20:41:08 +0900

    models.py import 수정

--- carts/models.py
+++ carts/models.py
@@ -1,11 +1,8 @@
 from django.db       import models
 
-from users.models    import User
-from products.models import Product, Storage
-
 class Cart(models.Model):
-    user       = models.ForeignKey('User', on_delete=models.CASCADE)
-    product    = models.ForeignKey('Product', on_delete=models.CASCADE)
+    user       = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    product    = models.ForeignKey('products.Product', on_delete=models.CASCADE)
     quantity   = models.PositiveIntegerField(default=1)
     is_active  = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
--- orders/models.py
+++ orders/models.py
@@ -1,11 +1,8 @@
 from django.db       import models
 
-from users.models    import User
-from products.models import Product
-
 class Order(models.Model):
-    user       = models.ForeignKey('User', on_delete=models.CASCADE)
-    cart       = models.ForeignKey('Cart', on_delete=models.CASCADE)
+    user       = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    cart       = models.ForeignKey('carts.Cart', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -14,7 +11,7 @@ class Order(models.Model):
 
 class OrderItem(models.Model):
     order      = models.ForeignKey('Order', on_delete=models.CASCADE)
-    product    = models.ForeignKey('Product', on_delete=models.CASCADE)
+    product    = models.ForeignKey('products.Product', on_delete=models.CASCADE)
     quantity   = models.PositiveIntegerField(default=1)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)

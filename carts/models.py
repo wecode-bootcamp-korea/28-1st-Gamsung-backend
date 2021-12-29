980a0c1b140cb708e73829430f4091d4ae23a44f
kang min sung <kangminsung@gangminseong-ui-MacBookPro.local>
2021-12-29 16:32:40 +0900

    [Modify] models in all apps

--- carts/models.py
+++ carts/models.py
@@ -1,11 +1,6 @@
 from django.db       import models
 
-class TimeStampModel(models.Model):
-    created_at  = models.DateTimeField(auto_now_add = True)
-    updated_at = models.DateTimeField(auto_now = True)
-
-    class Meta:
-        abstract = True
+from core.models import TimeStampModel
 
 class Cart(TimeStampModel):
     user       = models.ForeignKey('users.User', on_delete=models.CASCADE)
--- users/models.py
+++ users/models.py
@@ -1,11 +1,6 @@
 from django.db import models
 
-class TimeStampModel(models.Model):
-    created_at  = models.DateTimeField(auto_now_add = True)
-    updated_at = models.DateTimeField(auto_now = True)
-
-    class Meta:
-        abstract = True
+from core.models import TimeStampModel
 
 class User(TimeStampModel):
     first_name    = models.CharField(max_length=50)

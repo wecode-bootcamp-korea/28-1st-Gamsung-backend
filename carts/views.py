cb05d31859c7d5070b5e8d93c8532e8053a10e97
Byeongyeon Jung <byeongyeonjung@Byeongyeonui-MacBookPro.local>
2022-01-03 23:13:13 +0900

    Add: 장바구니 POST, GET 구현

--- carts/views.py
+++ carts/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+import json
+from django.shortcuts       import render
 
-# Create your views here.
+from django.http            import JsonResponse
+from django.views           import View
+
+from .models                import Cart
+from utils.login_required   import login_required
+
+class CartView(View):
+    @login_required
+    def get(self, request):
+
+        carts = Cart.objects.select_related('user__product').filter(user_id = request.user_id)
+        catalogue = {
+            "cart_list" : [{
+                "id"         : cart.id,
+                "product_id" : cart.product.id,
+                "price"      : cart.product.price,
+                "quantity"   : cart.quantity,
+                "detail_url" : cart.product.detail_url,
+            }for cart in carts]
+        }
+        return JsonResponse({"cart_list" : catalogue}, status=200)
+
+    @login_required
+    def post(self, request):
+        data       = json.loads(request.body)
+        user_id    = request.user.id
+        product_id = data['product_id']
+        quantity   = data['quantity']
+        cart, created = Cart.objects.get_or_create(
+            user_id    = user_id,
+            product_id = product_id
+        )            
+        cart.quantity += quantity
+        cart.save()
+        return JsonResponse({"message" : "SUCCESS"}, status=200)

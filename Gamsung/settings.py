93f3d8df1e0198259fa40822fed9b04834ad31a5
Byeongyeon Jung <byeongyeonjung@Byeongyeonui-MacBookPro.local>
2022-01-06 19:13:30 +0900

    Edit: settings.py 수정

--- Gamsung/settings.py
+++ Gamsung/settings.py
@@ -13,9 +13,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib     import Path
 from my_settings import SECRET_KEY, DATABASES
 
-# import pymysql
+import pymysql
 
-# pymysql.install_as_MySQLdb()
+pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
